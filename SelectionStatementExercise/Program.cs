b8f9e511c219d381b67f85fbdb42b2bd39270549
sumeyahajji <abdinasirsumeya@gmail.com>
2023-09-08 07:26:21 +0300

    completed exercise1 on selection statements

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            
+            var a = new Random();//used for generating random numbers
+            var favNumber = a.Next(20, 30);
+            
+
+            Console.WriteLine("Guess a random number:");//It prompts the user to input their guess.
+            var userInput = int.Parse(Console.ReadLine());//reads user input and convert it into integer and stores it in variable called userinput
+
+            if (favNumber < userInput)
+            {
+                Console.WriteLine("Too low!");
+
+            }
+            else if ( favNumber > userInput)
+            {
+                Console.WriteLine("Too high");
+            }
+
+            else
+            {
+                Console.WriteLine("Congratulation you guessed it well!");
+            }
+
+
+
+
         }
     }
 }
